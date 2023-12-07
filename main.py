e612604b775322d6a3ed8d76b935a4ded1359bd9
Kartik Naik <21890.kartik.sdcce@vvm.edu.in>
2023-12-07 19:03:49 +0530

    inspect and word search

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtWebEngineWidgets import *
 from PyQt5.QtGui import *
+from PyQt5.QtNetwork import *
 import requests
 from Classes.Chatbot import CustomChatbot
 from Classes.BookmarksManager import BookmarksManager
